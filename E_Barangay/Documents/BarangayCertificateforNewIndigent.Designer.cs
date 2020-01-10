919e6bf2182685708cbe2e9b40afeedfc9da079d
Yumaalseif <59075306+Yumaalseif@users.noreply.github.com>
2020-01-10 12:32:40 +0800

    Update BarangayCertificateforNewIndigent.Designer.cs

--- E_Barangay/Documents/BarangayCertificateforNewIndigent.Designer.cs
+++ E_Barangay/Documents/BarangayCertificateforNewIndigent.Designer.cs
@@ -404,7 +404,6 @@
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "BarangayCertificateforNewIndigent";
-            this.Text = "BarangayCertificateforNewIndigent";
             this.panel2.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
