46e6b2c42cef0e9857f43f7ab6d3a9f2dc5e0ac2
WoodsPentagon <50472091+WoodsPentagon@users.noreply.github.com>
2020-03-14 22:35:25 +0800

    Update BrgyCertDependent.cs

--- E_Barangay/Documents/BrgyCertDependent.cs
+++ E_Barangay/Documents/BrgyCertDependent.cs
@@ -50,10 +50,7 @@ namespace E_Barangay.Documents
         public override void Printing_PrintPage(object sender, PrintPageEventArgs e)
         {
             e.Graphics.DrawImage(E_Barangay.Properties.Resources.BarangayCertDEPENDENT, new PointF(0, 0));
-            ///Rectangle rect = new Rectangle(e.PageBounds.Width / 3 - 30, e.PageBounds.Height / 3 + 50, 548, 320);
             InitBodyRect(e);
-            //string name = Printing.IfControlEmpty(firstName);
-            //var h = new Class.NameHelper(name);
             string name = Printing.GetFullName(firstName, middleName, lastName, extension);
             string text = "To whom it may concern:" + Printing.LineSpace +
                            Printing.Indention + "This is to certify that " + name + " of legal age, Filipino and a resident of " + Printing.IfControlEmpty(address) + "." + Printing.LineSpace +
@@ -63,26 +60,6 @@ namespace E_Barangay.Documents
             e.Graphics.DrawString(text, Printing.font, Brushes.Black, rect);
             DrawDebugRecs(rect, e);
             Printing.DrawCapSb(e, o.captName, officerOption.Text);
-            //SizeF capBoxSize = e.Graphics.MeasureString(o.captName, Printing.fontBold);
-            //SizeF titleBoxSize = e.Graphics.MeasureString(o.captName, Printing.font);
-            //StringFormat format = new StringFormat();
-            //format.Alignment = StringAlignment.Center;
-
-            //Rectangle captRec = new Rectangle(e.PageBounds.Width * 3 / 5 + 20, e.PageBounds.Height * 2 / 3 + 40, 270, (int)capBoxSize.Height);
-            //e.Graphics.DrawString(o.captName.ToUpper(), Printing.fontBold, Brushes.Black, captRec, format);
-            //DrawDebugRecs(captRec, e);
-
-            //Rectangle capRecTitle = new Rectangle(e.PageBounds.Width * 3 / 5 + 20, captRec.Top + (int)capBoxSize.Height, 270, (int)titleBoxSize.Height * 2);
-            //e.Graphics.DrawString("Punong Barangay", Printing.font, Brushes.Black, capRecTitle, format);
-            //DrawDebugRecs(capRecTitle, e);
-
-            //Rectangle sbRect = new Rectangle(e.PageBounds.Width * 3 / 5 + 20, capRecTitle.Bottom + 30, 270, (int)capBoxSize.Height);
-            //e.Graphics.DrawString(Printing.IfControlEmpty(officerOption).ToUpper(), Printing.fontBold, Brushes.Black, sbRect, format);
-            //DrawDebugRecs(sbRect, e);
-
-            //Rectangle sbRectTitle = new Rectangle(e.PageBounds.Width * 3 / 5 + 20, sbRect.Top + (int)capBoxSize.Height, 270, (int)titleBoxSize.Height * 2);
-            //e.Graphics.DrawString("Sangguniang Barangay Member Officer of the Day", Printing.font, Brushes.Black, sbRectTitle, format);
-            //DrawDebugRecs(sbRectTitle, e);
         }
     }
 }
