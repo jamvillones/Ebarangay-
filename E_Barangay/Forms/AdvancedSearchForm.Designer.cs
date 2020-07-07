bfc10ab2480c2a0a21faa5565b16381f99495d19
WoodsPentagon <50472091+WoodsPentagon@users.noreply.github.com>
2020-07-07 19:07:25 +0800

    Update AdvancedSearchForm.Designer.cs

--- E_Barangay/Forms/AdvancedSearchForm.Designer.cs
+++ E_Barangay/Forms/AdvancedSearchForm.Designer.cs
@@ -33,25 +33,23 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.DataTable = new E_Barangay.Class.CustomDataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.isStudent = new System.Windows.Forms.CheckBox();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.isPwd = new System.Windows.Forms.CheckBox();
-            this.isSeniorCitizen = new System.Windows.Forms.CheckBox();
             this.isIndigent = new System.Windows.Forms.CheckBox();
+            this.isSeniorCitizen = new System.Windows.Forms.CheckBox();
+            this.isPwd = new System.Windows.Forms.CheckBox();
             this.areaOptions = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.sexOption = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
             this.civilStatusOption = new System.Windows.Forms.ComboBox();
-            this.panel2 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
-            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.DataTable)).BeginInit();
-            this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // DataTable
@@ -66,7 +64,7 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.DataTable.BackgroundColor = System.Drawing.SystemColors.ControlDark;
-            this.DataTable.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.DataTable.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.DataTable.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
             this.DataTable.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
             dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
@@ -91,7 +89,7 @@
             this.DataTable.DefaultCellStyle = dataGridViewCellStyle3;
             this.DataTable.EnableHeadersVisualStyles = false;
             this.DataTable.GridColor = System.Drawing.Color.White;
-            this.DataTable.Location = new System.Drawing.Point(12, 118);
+            this.DataTable.Location = new System.Drawing.Point(12, 129);
             this.DataTable.MultiSelect = false;
             this.DataTable.Name = "DataTable";
             this.DataTable.ReadOnly = true;
@@ -105,136 +103,9 @@
             this.DataTable.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
             this.DataTable.RowHeadersVisible = false;
             this.DataTable.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.DataTable.Size = new System.Drawing.Size(776, 320);
+            this.DataTable.Size = new System.Drawing.Size(776, 309);
             this.DataTable.StandardTab = true;
             this.DataTable.TabIndex = 3;
-            // 
-            // isStudent
-            // 
-            this.isStudent.AutoSize = true;
-            this.isStudent.Location = new System.Drawing.Point(3, 3);
-            this.isStudent.Name = "isStudent";
-            this.isStudent.Size = new System.Drawing.Size(63, 17);
-            this.isStudent.TabIndex = 4;
-            this.isStudent.Text = "Student";
-            this.isStudent.UseVisualStyleBackColor = true;
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.panel1.Controls.Add(this.isIndigent);
-            this.panel1.Controls.Add(this.isSeniorCitizen);
-            this.panel1.Controls.Add(this.isPwd);
-            this.panel1.Controls.Add(this.isStudent);
-            this.panel1.Location = new System.Drawing.Point(219, 12);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(95, 100);
-            this.panel1.TabIndex = 5;
-            // 
-            // isPwd
-            // 
-            this.isPwd.AutoSize = true;
-            this.isPwd.Location = new System.Drawing.Point(3, 26);
-            this.isPwd.Name = "isPwd";
-            this.isPwd.Size = new System.Drawing.Size(52, 17);
-            this.isPwd.TabIndex = 5;
-            this.isPwd.Text = "PWD";
-            this.isPwd.UseVisualStyleBackColor = true;
-            // 
-            // isSeniorCitizen
-            // 
-            this.isSeniorCitizen.AutoSize = true;
-            this.isSeniorCitizen.Location = new System.Drawing.Point(3, 49);
-            this.isSeniorCitizen.Name = "isSeniorCitizen";
-            this.isSeniorCitizen.Size = new System.Drawing.Size(90, 17);
-            this.isSeniorCitizen.TabIndex = 6;
-            this.isSeniorCitizen.Text = "Senior Citizen";
-            this.isSeniorCitizen.UseVisualStyleBackColor = true;
-            // 
-            // isIndigent
-            // 
-            this.isIndigent.AutoSize = true;
-            this.isIndigent.Location = new System.Drawing.Point(3, 72);
-            this.isIndigent.Name = "isIndigent";
-            this.isIndigent.Size = new System.Drawing.Size(64, 17);
-            this.isIndigent.TabIndex = 7;
-            this.isIndigent.Text = "Indigent";
-            this.isIndigent.UseVisualStyleBackColor = true;
-            // 
-            // areaOptions
-            // 
-            this.areaOptions.FormattingEnabled = true;
-            this.areaOptions.Location = new System.Drawing.Point(72, 10);
-            this.areaOptions.Name = "areaOptions";
-            this.areaOptions.Size = new System.Drawing.Size(121, 21);
-            this.areaOptions.TabIndex = 6;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(37, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(29, 13);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "Area";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(37, 40);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(25, 13);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "Sex";
-            // 
-            // sexOption
-            // 
-            this.sexOption.FormattingEnabled = true;
-            this.sexOption.Location = new System.Drawing.Point(72, 37);
-            this.sexOption.Name = "sexOption";
-            this.sexOption.Size = new System.Drawing.Size(121, 21);
-            this.sexOption.TabIndex = 8;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(7, 67);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(59, 13);
-            this.label3.TabIndex = 11;
-            this.label3.Text = "Civil Status";
-            // 
-            // civilStatusOption
-            // 
-            this.civilStatusOption.FormattingEnabled = true;
-            this.civilStatusOption.Location = new System.Drawing.Point(72, 64);
-            this.civilStatusOption.Name = "civilStatusOption";
-            this.civilStatusOption.Size = new System.Drawing.Size(121, 21);
-            this.civilStatusOption.TabIndex = 10;
-            // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.panel2.Controls.Add(this.civilStatusOption);
-            this.panel2.Controls.Add(this.label3);
-            this.panel2.Controls.Add(this.areaOptions);
-            this.panel2.Controls.Add(this.label1);
-            this.panel2.Controls.Add(this.label2);
-            this.panel2.Controls.Add(this.sexOption);
-            this.panel2.Location = new System.Drawing.Point(12, 12);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(201, 100);
-            this.panel2.TabIndex = 12;
-            // 
-            // button1
-            // 
-            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.button1.Location = new System.Drawing.Point(320, 49);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(226, 31);
-            this.button1.TabIndex = 13;
-            this.button1.Text = "Create Comma Separated Value(CSV)";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // Column1
             // 
@@ -255,25 +126,145 @@
             this.Column2.ReadOnly = true;
             this.Column2.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             // 
+            // isStudent
+            // 
+            this.isStudent.AutoSize = true;
+            this.isStudent.Location = new System.Drawing.Point(204, 15);
+            this.isStudent.Name = "isStudent";
+            this.isStudent.Size = new System.Drawing.Size(63, 17);
+            this.isStudent.TabIndex = 4;
+            this.isStudent.Text = "Student";
+            this.isStudent.UseVisualStyleBackColor = true;
+            // 
+            // isIndigent
+            // 
+            this.isIndigent.AutoSize = true;
+            this.isIndigent.Location = new System.Drawing.Point(273, 15);
+            this.isIndigent.Name = "isIndigent";
+            this.isIndigent.Size = new System.Drawing.Size(64, 17);
+            this.isIndigent.TabIndex = 7;
+            this.isIndigent.Text = "Indigent";
+            this.isIndigent.UseVisualStyleBackColor = true;
+            // 
+            // isSeniorCitizen
+            // 
+            this.isSeniorCitizen.AutoSize = true;
+            this.isSeniorCitizen.Location = new System.Drawing.Point(204, 69);
+            this.isSeniorCitizen.Name = "isSeniorCitizen";
+            this.isSeniorCitizen.Size = new System.Drawing.Size(90, 17);
+            this.isSeniorCitizen.TabIndex = 6;
+            this.isSeniorCitizen.Text = "Senior Citizen";
+            this.isSeniorCitizen.UseVisualStyleBackColor = true;
+            // 
+            // isPwd
+            // 
+            this.isPwd.AutoSize = true;
+            this.isPwd.Location = new System.Drawing.Point(204, 42);
+            this.isPwd.Name = "isPwd";
+            this.isPwd.Size = new System.Drawing.Size(52, 17);
+            this.isPwd.TabIndex = 5;
+            this.isPwd.Text = "PWD";
+            this.isPwd.UseVisualStyleBackColor = true;
+            // 
+            // areaOptions
+            // 
+            this.areaOptions.FormattingEnabled = true;
+            this.areaOptions.Location = new System.Drawing.Point(77, 13);
+            this.areaOptions.Name = "areaOptions";
+            this.areaOptions.Size = new System.Drawing.Size(121, 21);
+            this.areaOptions.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Area";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Sex";
+            // 
+            // sexOption
+            // 
+            this.sexOption.FormattingEnabled = true;
+            this.sexOption.Location = new System.Drawing.Point(77, 40);
+            this.sexOption.Name = "sexOption";
+            this.sexOption.Size = new System.Drawing.Size(121, 21);
+            this.sexOption.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Civil Status";
+            // 
+            // civilStatusOption
+            // 
+            this.civilStatusOption.FormattingEnabled = true;
+            this.civilStatusOption.Location = new System.Drawing.Point(77, 67);
+            this.civilStatusOption.Name = "civilStatusOption";
+            this.civilStatusOption.Size = new System.Drawing.Size(121, 21);
+            this.civilStatusOption.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(589, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(199, 31);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Create Comma Separated Value(CSV)";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // button2
             // 
-            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.button2.Location = new System.Drawing.Point(320, 12);
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(589, 12);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(226, 31);
+            this.button2.Size = new System.Drawing.Size(199, 31);
             this.button2.TabIndex = 14;
-            this.button2.Text = "Go";
+            this.button2.Text = "Search";
             this.button2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.groupBox1.Controls.Add(this.isIndigent);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.civilStatusOption);
+            this.groupBox1.Controls.Add(this.sexOption);
+            this.groupBox1.Controls.Add(this.isSeniorCitizen);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.isStudent);
+            this.groupBox1.Controls.Add(this.isPwd);
+            this.groupBox1.Controls.Add(this.areaOptions);
+            this.groupBox1.Location = new System.Drawing.Point(13, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(570, 111);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Filter";
             // 
             // AdvancedSearchForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
             this.Controls.Add(this.DataTable);
             this.Name = "AdvancedSearchForm";
             this.ShowIcon = false;
@@ -281,10 +272,8 @@
             this.Text = "Advanced Search";
             this.Load += new System.EventHandler(this.AdvancedSearchForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.DataTable)).EndInit();
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -293,7 +282,6 @@
 
         private Class.CustomDataGridView DataTable;
         private System.Windows.Forms.CheckBox isStudent;
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.CheckBox isIndigent;
         private System.Windows.Forms.CheckBox isSeniorCitizen;
         private System.Windows.Forms.CheckBox isPwd;
@@ -303,10 +291,10 @@
         private System.Windows.Forms.ComboBox sexOption;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.ComboBox civilStatusOption;
-        private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
